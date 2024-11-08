2e9994c0d97ffe927209ebb74974fa79fc4325ff
Falak-17 <falakahsan2005@gmail.com>
2024-11-08 20:22:55 +0400

    Add files via upload

---
+++ Simple sums.py
@@ -0,0 +1,11 @@
+#Exercise 02: Simple sums
+"""#1. Declare a variable and initialize it with the integer value 8.
+#2. Declare a second variable and initialize it with the integer value 10.
+#3. Declare a third variable that stores the sum of first two numbers.
+#4. Print the value of the sum to the console."""
+
+x=8    #declaring first variable
+y=10   #declaring second variable
+z=x+y  #storing sum in third variable
+
+print("Sum is",z)   #printing the sum
