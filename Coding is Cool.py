b99c955f78e9d4ca6e38890989a21c01ae3ea058
Falak-17 <falakahsan2005@gmail.com>
2024-11-08 20:22:26 +0400

    Add files via upload

---
+++ Coding is Cool.py
@@ -0,0 +1,8 @@
+#Exercise 01:Coding is Cool
+"""#Using string concatenation to combine the varisbles and print the phrase"""
+
+Word1="Coding "  #1st variable
+Word2="is "      #2nd variable
+Word3="Cool"     #3rd variable
+
+print(Word1+Word2+Word3)   #Using string concatenation and printing the phrase
