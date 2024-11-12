6327707e713fd96915ce76f3a87c9d10e978a7d4
Falak-17 <falakahsan2005@gmail.com>
2024-11-12 11:55:47 +0400

    Ex 03 Biography.py

--- Ex 03 Biography.py
+++ Ex 03 Biography.py
@@ -3,5 +3,5 @@
 #2. Print the values on separate lines using a single print() statement.
 #3. Use variables with appropriate data types for each piece of information"""
 
-person_info = {"name":"John", "hometown": "New York", "age":21}                     #creating a dictionary
-print(person_info["name"],person_info["hometown"],person_info["age"],sep="\n")      #printing all the values on seperate lines
+person_info = {"name":"John", "hometown": "New York", "age":21}                                    #creating a dictionary
+print(str(person_info["name"]),str(person_info["hometown"]),int(person_info["age"]),sep="\n")      #printing all the values on seperate lines
