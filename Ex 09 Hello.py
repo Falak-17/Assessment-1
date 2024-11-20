8f181c95d97aefcb25cd1868f2e286c0231450b5
Falak-17 <falakahsan2005@gmail.com>
2024-11-20 11:21:05 +0400

    Add files via upload

---
+++ Ex 09 Hello.py
@@ -0,0 +1,18 @@
+#Exercise 09: Hello
+"""#def hello():
+____  # Fill in this blank to print "Hello" to the console
+
+def main():
+____  # Fill in this blank to call the hello() function
+
+if __name__ == "__main__":
+main()"""
+
+def hello():
+    print("Hello")               #printing hello
+
+def main():
+    hello()                      #calling hello() function
+
+if __name__ == "__main__":   
+    main()                       #calling main function
