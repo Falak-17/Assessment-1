b3145dde2710db5ca1dbca142b8bec158e35dc9c
Falak-17 <falakahsan2005@gmail.com>
2024-11-20 11:19:08 +0400

    Update Ex 06 Brute Force Attack.py

--- Ex 06 Brute Force Attack.py
+++ Ex 06 Brute Force Attack.py
@@ -3,8 +3,8 @@
 #2. Use a while loop to repeatedly ask the user for the password until the correct one is entered.
 #3. Output an appropriate message when the correct password is entered."""
 
-password="12345"                          #Declare the password
-while password=="12345":                  #Using while loop
+password="12345"                            #Declare the password
+while password=="12345":                    #Using while loop
     password=input("Enter password: ")      #Asking user to input the corrct password
     if password=="12345":
        print("Access granted")              #If condition is fulfilled, access granted
